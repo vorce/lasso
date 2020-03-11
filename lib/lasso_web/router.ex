3122dcc440a68dc1663cacc1e1a94e95b3c6d688
Joel C <joel@vorce.se>
2020-03-11 22:08:58 +0100

    Adapt to 0.8.0 liveview backwards incompatible changes

--- lib/lasso_web/router.ex
+++ lib/lasso_web/router.ex
@@ -1,11 +1,11 @@
 defmodule LassoWeb.Router do
   use LassoWeb, :router
+  import Phoenix.LiveView.Router
 
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
-    plug :fetch_flash
-    plug Phoenix.LiveView.Flash
+    plug :fetch_live_flash
     plug :protect_from_forgery
 
     plug :put_secure_browser_headers, %{
