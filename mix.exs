8f95ffaa76d2c205cf526099e32d437db934e659
Joel C <joel@vorce.se>
2020-06-01 15:58:37 +0200

    Upgrade to elixir 1.10

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Lasso.MixProject do
     [
       app: :lasso,
       version: "1.4.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       elixirc_options: [warnings_as_errors: true],
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
