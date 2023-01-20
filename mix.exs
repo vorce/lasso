c7ca40a7c18c18b1115a1d50534610cdf4cde7af
Joel C <joel@vorce.se>
2023-01-20 12:48:55 +0100

    Upgrade elixir, erlang and node versions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Lasso.MixProject do
     [
       app: :lasso,
       version: "1.5.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       elixirc_options: [warnings_as_errors: true],
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
